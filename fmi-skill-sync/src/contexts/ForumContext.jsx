ac02224f7392c59ee08cdd6cb4fc2c764bdc0c66
MartiHr <81310884+MartiHr@users.noreply.github.com>
2024-03-15 22:00:39 +0200

    Create ForumContext.jsx

---
+++ fmi-skill-sync/src/contexts/ForumContext.jsx
@@ -0,0 +1,59 @@
+import { createContext, useContext,useEffect, useState } from "react";
+import * as forumService from "../services/forumService.js"
+import { useNavigate } from "react-router-dom";
+
+export const ForumContext = createContext();
+
+export const ForumProvider = ({
+    children,
+}) => {
+
+    const navigate = useNavigate();
+    const [topics, setTopics] = useState([]);
+
+    useEffect(() => {
+         forumService.getAllTopics()
+            .then(result => setTopics(result.slice()));
+    }, [])
+
+    const topicCreate = (topicData) => {
+        setTopics([topicData, ...topics]);
+        navigate('/forum');
+    }
+
+    const topicDelete = (topicId) => {
+        return setTopics(topics => topics.filter(t => t.id !== topicId));
+    }
+
+    const topicSelect = (topicId) => {
+        return topics.find(t => t.id === topicId) || {};
+    };
+
+    const topicDetails = (topicId, data) => {
+        return topics.map(t => t.id === topicId ? data : t);
+    }
+
+    const topicEdit = (topicId, topicData) => {
+        return setTopics(topics.map(t => t.id === topicId ? topicData : t));
+    }
+
+
+    return (
+        <ForumContext.Provider value={{
+            topics,
+            topicCreate,
+            topicDelete,
+            topicDetails,
+            topicEdit,
+            topicSelect,
+        }}>
+            {children}
+        </ForumContext.Provider>  
+    );
+};
+
+export const useForumContext = () => {
+    const context = useContext(ForumContext);
+
+    return context;
+};
