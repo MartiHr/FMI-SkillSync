0b070ee7a35a4ab7a6628761530aaf2a26bd9598
RadinTiholov <radin.tiholov@mail.bg>
2024-03-15 22:26:47 +0200

    Add function for time calculation

---
+++ fmi-skill-sync/src/utils/calculateTime.js
@@ -0,0 +1,32 @@
+export const calculateTime = (createdAt) => {
+    let dateNowSeconds = Date.now() / 1000;
+    let createdSeconds = createdAt.seconds;
+    let resSeconds = Math.floor(dateNowSeconds - createdSeconds);
+
+    if (resSeconds < 60) {
+        return `${resSeconds}s`;
+    }
+
+    let resMin = Math.floor(resSeconds / 60);
+
+    if (resMin < 60) {
+        return `${resMin}m`;
+    }
+
+    let resHours = Math.floor(resMin / 60);
+
+    if (resHours < 24) {
+        return `${resHours}h`;
+    }
+
+    let resDays = Math.floor(resHours / 24);
+
+    if (resDays < 365) {
+        return `${resDays}d`;
+    }
+
+    let resYears = Math.floor(resDays / 365);
+
+    return `${resYears}y`;
+
+}
