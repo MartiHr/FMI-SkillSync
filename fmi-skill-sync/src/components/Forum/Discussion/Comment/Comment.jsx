5fe18753dee3381ac4f2ef7d2ee4f9c94d78bfad
Vladimir Kotsev <v.kotsev04@gmail.com>
2024-03-15 21:40:00 +0200

    Add comment

---
+++ fmi-skill-sync/src/components/Forum/Discussion/Comment/Comment.jsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames/bind';
+import styles from './Comment.module.css';
+import { calculateTime } from '../../../../utils/calculateTime';
+
+let cx = classNames.bind(styles);
+
+export const Comment = ( {comment} ) => {
+
+    const {name, createdAt, description} = comment;
+
+    return (
+        <div className={cx('comment')}>
+            <img src="https://i.imgur.com/CFpa3nK.jpg" alt="Profile picture"/>
+            <div className={cx('comment-details')}>
+                <h4>{name}</h4>
+                <span> {calculateTime(createdAt)} </span>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
