90261b2afc7660b5e43dcfe3ad8fd08f7ee8f037
Vladimir Kotsev <v.kotsev04@gmail.com>
2024-03-15 21:21:19 +0200

    Adding Forum

---
+++ fmi-skill-sync/src/components/Forum/Forum.jsx
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Forum.module.css';
+import { Topic } from './Topic/Topic';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { ForumContext } from '../../contexts/ForumContext';
+
+
+let cx = classNames.bind(styles);
+
+export const Forum = () => {
+
+    const { topics } = useContext(ForumContext);
+
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedSubject, setSelectedSubject] = useState('all');
+
+    const subjects = ['DIS1', 'DIS2', 'AUDI', 'MINKO'];
+
+    const handleSubjectChange = (event) => {
+        setSelectedSubject(event.target.value);
+    };
+
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const filteredTopics = topics.filter((topic) =>
+        (selectedSubject === 'all' || topic.subject.toLowerCase() === selectedSubject.toLowerCase()) &&
+        topic.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+
+    return (
+        <div className={cx('forum')}>
+            <div className={cx('forum-heading')}>
+                <h1 className={cx('forum-heading-title')}>
+                    Welcome to our forum!
+                </h1>
+
+                <div className={cx('forum-search')}>
+                    <label>Search</label>
+                    <input
+                        className={cx('forum-topics-search')}
+                        type="text"
+                        placeholder="Search topics..."
+                        value={searchQuery}
+                        onChange={handleSearchChange}
+                    />
+                </div>
+
+                <div className={cx('select-dropdown')}>
+                    <label>Subject</label>
+                    <select
+                        className={cx('select-subject')}
+                        value={selectedSubject}
+                        onChange={handleSubjectChange}
+                    >
+                        {subjects.map((subject, index) => (
+                            <option key={index} value={subject.toLowerCase()}>
+                                {subject}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                <li className={cx('create-topic')}>
+                    <i className={cx('header-icon', 'fa-solid', 'fa-play')}></i>
+                    <Link to="/createTopic">Start a discussion</Link>
+                </li>
+            </div>
+            <div className={cx('topics-container')}>
+                {filteredTopics.map((filteredTopic, index) => (
+                    <Topic key={index} topic={filteredTopic} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
