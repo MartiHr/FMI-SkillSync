743422c1dc7fcf4aaee2ad07bfbe5e03bccdcb73
Vladimir Kotsev <v.kotsev04@gmail.com>
2024-03-15 22:43:41 +0200

    Changes to discussion

--- fmi-skill-sync/src/components/Forum/Discussion/Discussion.jsx
+++ fmi-skill-sync/src/components/Forum/Discussion/Discussion.jsx
@@ -5,56 +5,59 @@ import styles from './Discussion.module.css';
 import { Comment } from './Comment/Comment.jsx'
 import { Publication } from './Publication/Publication.jsx';
 import { useForumContext } from '../../../contexts/ForumContext';
+import * as forumService from '../../../services/forumService.js'
+import { useAuthContext } from '../../../contexts/AuthContext.jsx'
 
 let cx = classNames.bind(styles);
 
 export const Discussion = () => {
 
-    const { topicSelect } = useForumContext();
+    const { currentUser } = useAuthContext();
+    const { topicSelect, topicEdit } = useForumContext();
     const { id } = useParams();
-    const topic = topicSelect(id);
+    const currentTopic = topicSelect(id);
+    const [description, setDescription] = useState('');
+
+    const changeHandler = (e) => {
+        setDescription(e.target.value);
+    }
+
+    const handleComment = (e) => {
+        e.preventDefault();
+
+        const comment = { comment: { description: description, name: "TestUser", ownerId: currentUser.uid } };
+        forumService.commentTopic(id, comment, currentTopic.comments)
+            .then(comments => topicEdit(id, { ...currentTopic, comments }));
+
+        setDescription('');
+    }
 
     return (
         <>
-        <div className={cx('comments-container')}>
-            <Publication key={id}></Publication>
-            <h1>Comments</h1>
-            <div className={cx('comments')}>
-                {/* <div className={cx('comment')}>
-                    <img src="https://i.imgur.com/yTFUilP.jpg" alt="Profile picture"/>
-                    <div className={cx('comment-details')}>
-                        <h4>Jhon Doe</h4>
-                        <span>- 20 October, 2018</span>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+            <div className={cx('comments-container')}>
+                <div className={cx('comment-post')}>
+                    <Publication key={id}></Publication>
+                    <div className={cx('comments')}>
+                        <h1>Comments</h1>
+                        {currentTopic.comments?.length > 0 ? currentTopic.comments.map((x, index) => <Comment key={index + 1} comment={x.comment} />)
+                            : <h1>No comments yet</h1>}
                     </div>
-                </div>
-                <div className={cx('comment')}>
-                    <img src="https://i.imgur.com/CFpa3nK.jpg" alt="Profile picture"/>
-                    <div className={cx('comment-details')}>
-                        <h4>Rob Simpson</h4>
-                        <span>- 20 October, 2018</span>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
-                    </div>
-                </div> */}
 
-                <h1>Comments</h1>
-                {topic.comments?.length > 0 ? topic.comments.map((x, index) => <Comment key={index + 1} comment={x.comment} />)
-                                            : <h1>No comments yet</h1>}
+                </div>
+                <div className={cx('comment-form')}>
+                    <form id="align-form" onSubmit={handleComment}>
+                        <h4>Leave a comment</h4>
+                        <div className={cx('form-group')}>
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="msg" cols="30" rows="5" placeholder="Write your comment.." value={description} onChange={changeHandler}></textarea>
+                            <span></span>
+                        </div>
+                        <div className={cx('form-group')}>
+                            <button>Post Comment</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className={cx('comment-form')}>
-                <form id="align-form">
-                    <h4>Leave a comment</h4>
-                    <div className={cx('form-group')}>
-                        <label for="message">Message</label>
-                        <textarea name="msg" id="msg" cols="30" rows="5" placeholder="Write your comment.."></textarea>
-                        <span></span>
-                    </div>
-                    <div className={cx('form-group')}>
-                        <button>Post Comment</button>
-                    </div>
-                </form>
-            </div>
-        </div>
         </>
     );
 }
