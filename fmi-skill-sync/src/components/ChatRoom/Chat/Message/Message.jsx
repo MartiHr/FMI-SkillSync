c96aa5b98c48afb63dbbf226f6d8bd8b97edb9ce
Daniel Stefanov <97540472+ludsmotika@users.noreply.github.com>
2024-03-15 23:18:00 +0200

    Add profile picture to register and firebase storage logic to upload image

--- fmi-skill-sync/src/components/ChatRoom/Chat/Message/Message.jsx
+++ fmi-skill-sync/src/components/ChatRoom/Chat/Message/Message.jsx
@@ -1,10 +1,20 @@
 import classNames from 'classnames/bind';
 import chatRooomStyles from './Message.module.css';
 import { extractUsernameFromEmail } from '../../../../utils/usernameUtils';
+import { getProfilePictureByEmail } from '../../../../services/userService.js';
+import { useEffect, useState } from 'react';
 
 let cx = classNames.bind(chatRooomStyles);
 
 export const Message = ({ isYou, text, from, date }) => {
+    const [urlPicture, setUrlPicture] = useState('');
+
+    useEffect(() => {
+        getProfilePictureByEmail()
+            .then(res => setUrlPicture(res))
+            .catch(err => console.log(err));
+    }, []);
+
     return (
         <>
             {isYou ?
@@ -20,13 +30,13 @@ export const Message = ({ isYou, text, from, date }) => {
                 <div className={cx("message-container")}>
                     <p>{extractUsernameFromEmail(from)} - {date.day}/{date.month}/{date.year} - {date.hours}:{date.minutes}:{date.seconds}</p>
                     <div className={cx("person-a")}>
-                        <div className={cx("icon")}></div> {/* icon*/}
+                        <img className={cx("icon")} src={urlPicture} />
                         <div className={cx("message")}>
                             {text}
                         </div>
                     </div>
                 </div>
-                }
+            }
         </>
     )
 }
