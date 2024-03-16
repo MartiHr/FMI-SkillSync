c4156ac2720652d3fd2326d46a398d9261a4f376
Vladimir Kotsev <v.kotsev04@gmail.com>
2024-03-16 22:41:33 +0200

    Update RadioButtonGroup.jsx

--- fmi-skill-sync/src/components/FormComponents/AcceptRequest/RadioButtonGroup.jsx
+++ fmi-skill-sync/src/components/FormComponents/AcceptRequest/RadioButtonGroup.jsx
@@ -8,22 +8,24 @@ export const RadioButtonGroup = (props) => {
         <>
             <div className={cx('image-wrapper')}>
                 <div>
+                    <label>Behind 325</label>
                     <img src="https://firebasestorage.googleapis.com/v0/b/fmi-codes-a71a1.appspot.com/o/images%2Fzad325.jpg?alt=media&token=50c0e5b0-ae74-4016-b465-881347f87464" alt="no photo" />
-                    <label><input type='radio' name="first" value="1" onChange={props.handleChange} checked={props.selectedOption === '1'} ></input>Behind 325</label>
+                    <input type='radio' name="first" value="1" onChange={props.handleChange} checked={props.selectedOption === '1'} ></input>
                 </div>
                 <div>
+                    <label>Behind 200</label>
                     <img src="https://firebasestorage.googleapis.com/v0/b/fmi-codes-a71a1.appspot.com/o/images%2Fzad200.jpg?alt=media&token=252f1868-bdc6-4b15-b344-5bd0c413941f" alt="no photo" />
-                    <label><input type='radio' name="second" value="2" onChange={props.handleChange} checked={props.selectedOption === '2'}></input>Behind 200</label>
-
+                    <input type='radio' name="second" value="2" onChange={props.handleChange} checked={props.selectedOption === '2'}></input>
                 </div>
                 <div>
+                    <label>The aquarium</label>
                     <img src="https://firebasestorage.googleapis.com/v0/b/fmi-codes-a71a1.appspot.com/o/images%2Faquarium.jpg?alt=media&token=dd0ff6fb-b098-4a9a-be0d-a0f422783b74" alt="no photo" />
-                    <label><input type='radio' name="third" value="3" onChange={props.handleChange} checked={props.selectedOption === '3'}></input>The aquarium</label>
-
+                    <input type='radio' name="third" value="3" onChange={props.handleChange} checked={props.selectedOption === '3'}></input>
                 </div>
                 <div>
+                    <label>The 6th floor</label>
                     <img src="https://firebasestorage.googleapis.com/v0/b/fmi-codes-a71a1.appspot.com/o/images%2F6tietaj.jpg?alt=media&token=1aaaee8a-b79c-4a68-8499-c1daaa7e8887" alt="no photo" />
-                    <label><input type='radio' name="fourth" value="4" onChange={props.handleChange} checked={props.selectedOption === '4'}></input>The 6th floor</label>
+                    <input type='radio' name="fourth" value="4" onChange={props.handleChange} checked={props.selectedOption === '4'}></input>
                 </div>
             </div>
         </>
