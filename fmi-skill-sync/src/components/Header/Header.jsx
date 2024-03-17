a2aad0f05a76b7cd30d96a668f0a08cd93cccd49
RadinTiholov <radin.tiholov@mail.bg>
2024-03-17 14:42:34 +0200

    Add set events to header

--- fmi-skill-sync/src/components/Header/Header.jsx
+++ fmi-skill-sync/src/components/Header/Header.jsx
@@ -59,6 +59,10 @@ export const Header = () => {
                             <Link to="/events">{t("EventsButtonText")}</Link>
                         </li>
                         <li>
+                            <i className={cx('header-icon', 'fa-solid', 'fa-calendar')} onClick={(event) => redirect('/events', event)}></i>
+                            <Link to="/set-events">{t("SetEventsButtonText")}</Link>
+                        </li>
+                        <li>
                             <i className={cx('header-icon', 'fa-solid', 'fa-comment')} onClick={(event) => redirect('/forum', event)}></i>
                             <Link to="/forum">{t("ForumButtonText")}</Link>
                         </li>
@@ -86,84 +90,3 @@ export const Header = () => {
         </header>
     );
 };
-
-
-
-
-// import classNames from 'classnames/bind';
-// import styles from './Header.module.css';
-// import { Link, useNavigate } from 'react-router-dom';
-// import logo from '../../../public/static/images/logo.png'
-// import { useContext, useState } from 'react';
-// import { AuthContext } from '../../contexts/AuthContext';
-// import { useTranslation } from "react-i18next";
-// import { LanguageSelector } from "../LanguageSelector/LanguageSelector";
-
-// let cx = classNames.bind(styles);
-
-// export const Header = () => {
-//     const { t } = useTranslation();
-
-//     //const { user } = useSelector((state) => state.auth);
-//     const { currentUser } = useContext(AuthContext);
-//     const [isOpen, setIsOpen] = useState(false);
-
-//     const openHeader = () => {
-//         setIsOpen((state) => !state)
-//     }
-
-//     const navigate = useNavigate();
-
-//     const redirect = (path, event) => {
-//         event.stopPropagation();
-//         navigate(path);
-//     }
-
-//     return (
-//         <header onClick={() => { return isOpen ? setIsOpen(false) : setIsOpen(true) }} className={cx('header', isOpen ? '' : 'header-closed')} >
-//             <ul className={cx('header-list')}>
-//                 <li className={cx('header-heading')}>
-//                     <img className={cx('header-logo')} src={logo} alt='The logo of the web application' />
-//                     <Link className={cx('header-heading-text')} to="/">FMI Skill Sync</Link>
-//                 </li>
-//                 <li>
-//                     <i className={cx('header-icon', 'fa-solid', 'fa-home')} onClick={(event) => redirect('/', event)}></i>
-//                     <Link to="/">{t("HomeButtonText")}</Link>
-//                 </li>
-//                 {currentUser ?
-//                     <>
-//                         {/* TODO: Remove me */}
-//                         <li>
-//                             <i className={cx('header-icon', 'fa-solid', 'fa-comment')} onClick={(event) => redirect('/chat-room/petyoebenared@gmail.com', event)}></i>
-//                             <Link to="/chat-room/kotsev@gmail.com">Chat (Remove)</Link>
-//                         </li>
-
-//                         {/* TODO: Remove me */}
-//                         <li>
-//                             <i className={cx('header-icon', 'fa-solid', 'fa-comment')} onClick={(event) => redirect('/events', event)}></i>
-//                             <Link to="/events">Events (Remove)</Link>
-//                         </li>
-//                         <li>
-//                             <i className={cx('header-icon', 'fa-solid', 'fa-comment')} onClick={(event) => redirect('/forum', event)}></i>
-//                             <Link to="/forum">{t("ForumButtonText")}</Link>
-//                         </li>
-//                         <li>
-//                             <i className={cx('header-icon', 'fa-solid', 'fa-door-open')} onClick={(event) => redirect('/logout', event)}></i>
-//                             <Link to="/logout">{t("LogoutButtonText")}</Link>
-//                         </li>
-//                     </>
-//                     :
-//                     <>
-//                         <li>
-//                             <i className={cx('header-icon', 'fa-solid', 'fa-play')} onClick={(event) => redirect('/register', event)}></i>
-//                             <Link to="/register">{t("StartNowButtonText")}</Link>
-//                         </li>
-//                         <li>
-//                             <i className={cx('header-icon', 'fa-solid', 'fa-right-to-bracket')} onClick={(event) => redirect('/login', event)}></i>
-//                             <Link to="/login">{t("LogoutButtonText")}</Link>
-//                         </li>
-//                     </>}
-//             </ul>
-//         </header>
-//     );
-// };
